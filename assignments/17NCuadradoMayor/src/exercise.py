aca807f96fe69536690d8d8642f86e191f627e98
VicenteDH <89059919+VicenteDH@users.noreply.github.com>
2021-09-07 20:05:08 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,8 +1,12 @@
-
-
 def main():
     #Escribe tu código debajo de esta línea
-    pass
-
+    Valor= int(input("Escribe un numero : "))
+    Vi=1
+    while Valor> Vi**2:
+        Vi=Vi+1
+        variable=Vi**2
+        if variable>Valor:
+            print(Vi)
+            
 if __name__=='__main__':
     main()
